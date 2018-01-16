8b0d0251882b83fa3a6f057b7512f69ccfff866a
Unknown <loic.dessaules@gmail.com>
2018-01-16 15:43:11 +0100

    Added a server TCP and a client in python to communicate with socket

---
+++ processors/client.py
@@ -0,0 +1,21 @@
+import socket
+
+host = "localhost"
+port = 12800
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((host, port))
+print("Connection with the server established on the port : {}".format(port))
+
+msgToSend = b""
+while msgToSend != b"fin":
+    msgToSend = input("> ")
+    # Peut planter si vous tapez des caractères spéciaux
+    msgToSend = msgToSend.encode()
+    # On envoie le message
+    socket.send(msgToSend)
+    rcvMsg = socket.recv(1024)
+    print(rcvMsg.decode()) # Là encore, peut planter s'il y a des accents
+
+print("Connection close")
+socket.close()
---
+++ processors/server.py
@@ -0,0 +1,50 @@
+import socket
+import select
+
+host = ''
+port = 12800
+
+# AF_INET for INET ADDRESS
+# SOCK_STREAM for TCP protocol
+mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mainSocket.bind((host, port))
+mainSocket.listen(5)
+print("Server listen port {}".format(port))
+
+server_run = True
+connectedClients = []
+while server_run:
+    # Verify each 0.05 (max) sec, if someone ask for connect (he sent a connection packet)
+    askedConnections, wList, xList = select.select([mainSocket], [], [], 0.05)
+    for connection in askedConnections:
+        clientSocket, clientInfos = connection.accept()
+        # Add the new accepted client socket to our connected clients
+        connectedClients.append(clientSocket)
+        print("One client connected")
+
+    clientsToListen = []
+    # Verify each 0.05 (max) sec, if a connected client is enabled to read (he sent a msg)
+    try:
+        clientsToListen, wList, xList = select.select(connectedClients, [], [], 0.05)
+    except select.error:
+        pass
+    else:
+        # Get clientSocket which have sent a msg one by one
+        for clientSocket in clientsToListen:
+            recvMsg = clientSocket.recv(1024)
+            recvMsg = recvMsg.decode()
+            if recvMsg == "fin":
+                clientSocket.close()
+                clientsToListen.remove(clientSocket)
+                connectedClients.remove(clientSocket)
+                print("One client disconnected")
+            elif recvMsg == "close-server":
+                server_run = False
+            else :
+                print("Msg received : {}".format(recvMsg))
+                clientSocket.send(b"5/5")
+
+print("Server close")
+for clientSocket in connectedClients:
+    clientSocket.close()
+mainSocket.close()
