969b63698726a9891cd1bd3b39854c460d33ef56
Eric Broutba <eric.bousbaa@hotmail.com>
2018-01-25 04:38:15 -0500

    Processor: try catch to close connexion if error

--- processors/server.py
+++ processors/server.py
@@ -16,36 +16,38 @@ processor = Processor()
 
 server_run = True
 connectedClients = []
-while server_run:
-    # Verify each 0.05 (max) sec, if someone ask for connect (he sent a connection packet)
-    askedConnections, wList, xList = select.select([mainSocket], [], [], 0.05)
-    for connection in askedConnections:
-        clientSocket, clientInfos = connection.accept()
-        # Add the new accepted client socket to our connected clients
-        connectedClients.append(clientSocket)
-        print("One client connected")
+try:
+    while server_run:
+        # Verify each 0.05 (max) sec, if someone ask for connect (he sent a connection packet)
+        askedConnections, wList, xList = select.select([mainSocket], [], [], 0.05)
+        for connection in askedConnections:
+            clientSocket, clientInfos = connection.accept()
+            # Add the new accepted client socket to our connected clients
+            connectedClients.append(clientSocket)
+            print("One client connected")
 
-    clientsToListen = []
-    # Verify each 0.05 (max) sec, if a connected client is enabled to read (he sent a msg)
-    try:
-        clientsToListen, wList, xList = select.select(connectedClients, [], [], 0.05)
-    except select.error:
-        pass
-    else:
-        # Get clientSocket which have sent a msg one by one
-        for clientSocket in clientsToListen:
-            recvMsg = clientSocket.recv(1024)
-            recvMsg = recvMsg.decode()
-            if recvMsg == "fin":
-                clientSocket.close()
-                clientsToListen.remove(clientSocket)
-                connectedClients.remove(clientSocket)
-                print("One client disconnected")
-            else:
-                print("server.py -> receive a msg")
-                processor.peel(recvMsg)
-                processor.execute(clientSocket)
-
+        clientsToListen = []
+        # Verify each 0.05 (max) sec, if a connected client is enabled to read (he sent a msg)
+        try:
+            clientsToListen, wList, xList = select.select(connectedClients, [], [], 0.05) 
+        except select.error:
+            pass
+        else:
+            # Get clientSocket which have sent a msg one by one
+            for clientSocket in clientsToListen:
+                recvMsg = clientSocket.recv(1024)
+                recvMsg = recvMsg.decode()
+                if recvMsg == "fin":
+                    clientSocket.close()
+                    clientsToListen.remove(clientSocket)
+                    connectedClients.remove(clientSocket)
+                    print("One client disconnected")
+                else:
+                    print("server.py -> receive a msg")
+                    processor.peel(recvMsg)
+                    processor.execute(clientSocket)
+except IndexError:
+    mainSocket.close()
 
 print("Server close")
 for clientSocket in connectedClients:
