bb44554e5b1864c799c11134d83d6d7b64f33c7f
Bastien <bastien.nicoud@gmail.com>
2018-11-26 20:47:46 +0100

    Link store to editor option for easy configuration

--- client/src/store.js
+++ client/src/store.js
@@ -5,10 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    /**
+     * Stores all editor related datas
+     */
+    editor: {
+      theme: 'vs-dark',
+      language: 'javascript',
+      // The value of the editor (the code)
+      value: 'console.log(tutu)'
+    }
   },
   mutations: {
-
+    UPDATE_EDITOR_VALUE: (state, value) => {
+      state.editor.value = value
+    }
   },
   actions: {
 
