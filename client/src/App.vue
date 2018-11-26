1e733ee4d1938e8d6dd2cb0fe632b0b1b216f4fe
Bastien <bastien.nicoud@gmail.com>
2018-11-26 18:47:00 +0100

    Styling navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,17 @@
 <script>
 import NavBar from '@/components/NavBar'
+import EditorBox from '@/components/editor/EditorBox'
+import GameBox from '@/components/game/GameBox'
+import ConsoleBox from '@/components/console/ConsoleBox'
 /**
  * Base Vue component
  */
 export default {
   components: {
-    NavBar
+    NavBar,
+    EditorBox,
+    GameBox,
+    ConsoleBox
   }
 }
 </script>
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,5 +1,13 @@
 <template>
   <nav id="nav-bar">
-    Navbar
+    <div class="nav-left">
+      tutu
+    </div>
+    <div class="nav-center">
+      <h1>LiveCoding</h1>
+    </div>
+    <div class="nav-right">
+      tata
+    </div>
   </nav>
 </template>
