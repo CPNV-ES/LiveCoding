71594aa06ad3941012053f0edfd39cac7ee5698d
CARBONI Davide <Davide.CARBONI@cpnv.ch>
2018-12-14 14:14:01 +0100

    modify manifest file

--- server/languages/targetLanguage/php/__manifest__.py
+++ server/languages/targetLanguage/php/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Php Language Class',
     'targetLanguage': 'Php',
--- server/languages/targetLanguage/ruby/__manifest__.py
+++ server/languages/targetLanguage/ruby/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Ruby Language Class',
     'targetLanguage': 'Ruby',
