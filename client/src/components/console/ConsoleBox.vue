3ba8df263d6ac716eac15555d74dc6ad693d53f3
Bastien <bastien.nicoud@gmail.com>
2018-12-14 11:20:04 +0100

    Adding layout to display the messages in the game console

--- client/src/components/console/ConsoleBox.vue
+++ client/src/components/console/ConsoleBox.vue
@@ -1,5 +1,30 @@
+<script>
+/**
+ * Game console
+ *
+ * @author Bastien Nicoud
+ */
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      messages: state => state.console.messages
+    })
+  }
+}
+</script>
+
 <template>
   <div id="console-box">
-    Console
+    <div
+      v-for="message in messages"
+      :key="message.id"
+      class="message"
+    >
+      <span :class="message.type">
+        {{ message.content }}
+      </span>
+    </div>
   </div>
 </template>
