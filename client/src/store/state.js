8624da890f2a8d86eff14a69a0bdcdef1b0f09bc
Bastien <bastien.nicoud@gmail.com>
2018-12-14 10:49:56 +0100

    Add key in the store to save game console messages.

--- client/src/store/state.js
+++ client/src/store/state.js
@@ -35,5 +35,34 @@ export default {
     url: 'http://localhost:3333',
     loaded: false,
     manager: null
+  },
+  /**
+   * Conosole stores the messages displayed in the little console on bottom of the page
+   */
+  console: {
+    /**
+     * Each messages will be displayed into the console of the browser, and the console of the game
+     * **Message datas**
+     * - **content :** the value to display
+     * - **type :** the type of message (log, info, warning, error, ...)
+     * - **time :** timestamp at the emission of the message
+     */
+    messages: [
+      {
+        content: 'Application launched',
+        type: 'info',
+        time: new Date()
+      },
+      {
+        content: 'Loading Game ...',
+        type: 'info',
+        time: new Date()
+      },
+      {
+        content: 'Error, game not accesible, check network.',
+        type: 'error',
+        time: new Date()
+      }
+    ]
   }
 }
