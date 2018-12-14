cf3cbbcdb569928da812ca4cb3cc31321c35fd3d
CARBONI Davide <davide.carboni@cpnv.ch>
2018-12-14 10:14:20 +0100

    change folder structure

---
+++ server/languages/targetLanguage/php/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Php Language Class',
+    'targetLanguage': 'Php',
+    'version': '1.0',
+    'category': 'Language',
+    'description': ''
+}
---
+++ server/languages/targetLanguage/ruby/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ruby Language Class',
+    'targetLanguage': 'Ruby',
+    'version': '1.0',
+    'category': 'Language',
+    'description': ''
+}
