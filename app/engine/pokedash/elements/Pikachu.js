17025c69a523203202cc83c80a1de02bd07066b0
Julien Richoz <julienrichoz@outlook.com>
2019-01-14 13:33:12 +0100

    Change pikachu name to protagonist

--- app/engine/pokedash/elements/Pikachu.js
+++
@@ -1,8 +0,0 @@
-class Pikachu extends MovableElement{
-    constructor(x, y, img){
-        super(x, y, img)
-    } 
-    //static LIFE = 3 
-}
-
-// TO DELETE IF NOT USED : module.exports = Pikachu
---
+++ app/engine/pokedash/elements/Protagonist.js
@@ -0,0 +1,13 @@
+class Protagonist extends MovableElement{
+    constructor(x, y, img){
+        super(x, y, img)
+    }
+
+    //static LIFE = 3 
+    /*moveLeft(){
+        console.log("Protagonist move left")
+        console.log(game.blockWidth);
+        this.x = this.x - game.blockWidth;
+        return true;
+    }*/
+}
