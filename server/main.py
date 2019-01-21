f153fa840a539fe3d1df2e77f3dbd9096f8639be
CARBONI Davide <Davide.CARBONI@cpnv.ch>
2019-01-21 10:20:09 +0100

    remove old config to use the new configuration file

--- server/main.py
+++ server/main.py
@@ -1,9 +1,13 @@
 from core.listen import Listen
 from core.connection import Connection
+from mlogging import mlog
 import socket
+import configparser
 
-host = ''
-port = 12800
+config = configparser.ConfigParser()
+config.read("config.ini")
+host = config.get("SOCKET CONFIGURATION","HOST")
+port = int(config.get("SOCKET CONFIGURATION","PORT"))
 
 connection = Connection(host, port)          # new socket connection
 listen = Listen(connection.mainSocket)       # start a new listening instance to get message from socket
