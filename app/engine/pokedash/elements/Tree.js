fb4740a68b7fb95a94b0307d042b0a0fd413b042
Julien Richoz <julienrichoz@outlook.com>
2019-01-21 08:34:18 +0100

    New conception idea: action on each element. Test worked

--- app/engine/pokedash/elements/Tree.js
+++ app/engine/pokedash/elements/Tree.js
@@ -2,6 +2,12 @@ class Tree extends StaticElement{
     constructor(x, y, img){
         super(x, y, img)
     }
+
+    action(){
+        console.log("bienvenue dans la méthode action de la classe Tree")
+        return true
+    }
+
 }
 
 // TO DELETE IF NOT USED : module.exports = Tree
