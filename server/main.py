b72d3816bfeaf86ae2b5d7d647df59f8b6541fa0
CARBONI Davide <davide.carboni@cpnv.ch>
2019-01-21 08:32:12 +0100

    change host address

--- server/main.py
+++ server/main.py
@@ -2,10 +2,10 @@ from core.listen import Listen
 from core.connection import Connection
 import socket
 
-host = 'localhost'
+host = ''
 port = 12800
 
 connection = Connection(host, port)          # new socket connection
-listen = Listen(connection.mainSocket)                  # start a new listening instance to get message from socket
+listen = Listen(connection.mainSocket)       # start a new listening instance to get message from socket
 listen.run()                                 # start server 
 
