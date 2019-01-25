7469b3990cadfad66a557d4677ebb8ddc62c5296
Julien Richoz <julienrichoz@outlook.com>
2019-01-25 14:44:47 +0100

    Handle road action

--- app/engine/pokedash/elements/Road.js
+++ app/engine/pokedash/elements/Road.js
@@ -3,7 +3,20 @@ class Road extends MovableElement{
         super(x, y, img)
     }
     action(information){
+        let temp = game.mapElement[this.posX][this.posY]
+        let tempX = this.posX
+        let tempY = this.posY
+        console.log(game.mapElement[this.posX][this.posY])
+        game.mapElement[this.posX][this.posY] = game.mapElement[game.playerPosX][game.playerPosY]
+        game.mapElement[game.playerPosX][game.playerPosY].moveSprite(information)
+        game.mapElement[game.playerPosX][game.playerPosY] = temp
+        this.posX = game.playerPosX
+        this.posY = game.playerPosY
+        game.playerPosX = tempX
+        game.playerPosY = tempY
         
+       
+
     }
 }
 
