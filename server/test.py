beb6a52b85b2dd6de8164803784dfec25b4a6397
CARBONI Davide <Davide.CARBONI@cpnv.ch>
2019-01-25 15:06:44 +0100

    test for new websocket closed

--- server/test.py
+++ server/test.py
@@ -9,31 +9,29 @@ from mlogging import mlog
 class MyWebSocketServer(tornado.websocket.WebSocketHandler):
 
     clients = set()
-    clientss = {}
 
     def open(self):
         # metodo eseguito all'apertura della connessione
-        mlog.show('New connection created for client %s' % self)
+        mlog.show('New connection created for client')
         MyWebSocketServer.clients.add(self)
     
-    @gen.coroutine
     def on_message(self, message):
         # metodo eseguito alla ricezione di un messaggio
         # la stringa 'message' rappresenta il messaggio
         mlog.show('Messaggio ricevuto: %s' % message)
         self.write_message("Received: " + message)
-        print(self)
-
+        
     def on_close(self):
         # metodo eseguito alla chiusura della connessione
-        mlog.show('Connection %s has been close ' %self)
+        mlog.show('Connection for a client has been closed')
         MyWebSocketServer.clients.remove(self)
 
     def check_origin(self, origin):
-        return
+        return True
 
-        
+mlog.show("Starting Live Coding Server .... Wait")
 application = tornado.web.Application([(r'/websocketserver', MyWebSocketServer),])
 http_server = tornado.httpserver.HTTPServer(application)
 http_server.listen(12800)
+mlog.show("Live Coding Server has been loaded.... Waiting for new connection")
 tornado.ioloop.IOLoop.instance().start()
