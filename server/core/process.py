c6e6aff2ebd615edbf7a71dd56df6d352324f259
CARBONI Davide <Davide.CARBONI@cpnv.ch>
2019-02-13 15:23:26 +0100

    BUG FIXED into run function when a error message is generate and

--- server/core/process.py
+++ server/core/process.py
@@ -23,15 +23,14 @@ class Process:
         self.process = subprocess.Popen(self.languageName + " " + self.tempfile.getName(), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         # Fetch the error from the stderr ("none" if no error)
         errorMsg = self.process.stderr.readline().decode()
-        
-        if (errorMsg == "none"):
+        if errorMsg.strip() == "none":
             self.proxyGame()
         else:
-            mlog.show("Process error..Game has been stopped..")
+            mlog.show("Process error.. Game has been stopped..")
             mlog.show("Error message: " + errorMsg)
             self.socket.send("ERROR/" + errorMsg)
-            return
         pass
     
     def proxyGame(self):
+        # wait for client reponse
         pass
