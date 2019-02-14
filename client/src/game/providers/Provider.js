b928ef30b1678e1670b979c20b2597bde21f6113
bastien <bastien.nicoud@gmail.com>
2019-02-14 14:06:54 +0100

    Add method in the provider to load the required libraries

--- client/src/game/providers/Provider.js
+++ client/src/game/providers/Provider.js
@@ -30,6 +30,7 @@ export default class Provider {
   get gameName () {
     return this.manifest.name
   }
+
   /**
    * Returns the name of the loaded game
    * @return {String}
@@ -37,6 +38,7 @@ export default class Provider {
   get gameDescription () {
     return this.manifest.description
   }
+
   /**
    * Returns the name of the loaded game
    * @return {String}
@@ -44,6 +46,7 @@ export default class Provider {
   get gameInterpreters () {
     return this.manifest.interpreters
   }
+
   /**
    * Returns the array of instructions
    * @return {Array}
@@ -51,6 +54,15 @@ export default class Provider {
   get gameInstructions () {
     return this.manifest.instructions
   }
+
+  /**
+   * Returns the array of libraries
+   * @return {Array}
+   */
+  get gameLibraries () {
+    return this.manifest.data.libraries
+  }
+
   /**
    * Return the base path of the game assets
    * @return {String}
@@ -58,6 +70,7 @@ export default class Provider {
   get assetsBasePath () {
     return `${this.url}/${this.manifest.data.assets}/`
   }
+
   /**
    * Generate an url to load a ressource
    * @param {String} file path of the file
@@ -66,6 +79,7 @@ export default class Provider {
   generateRawUrl (file) {
     return `${this.url}/${file}`
   }
+
   /**
    * Generate an url to display a ressource
    * @param {String} file path of the file
@@ -74,6 +88,7 @@ export default class Provider {
   generateUrl (file) {
     return `${this.url}/${file}`
   }
+
   /**
    * Loads the manifest of the specified game
    * @async
@@ -88,15 +103,32 @@ export default class Provider {
       throw new Error('Impossible to load the game manifest, check your url, or if a manifest is present.')
     }
   }
+
   /**
    * Load the code for each available interpreters
    */
   async loadInterpreters () {
+    // Fetch the interpreters for each key referenced in the manifest
     for (let interpreter in this.gameInterpreters) {
       let response = await fetch(this.generateRawUrl(this.gameInterpreters[interpreter]))
       this.interpreters[interpreter] = await response.text()
     }
   }
+
+  /**
+   * Load the libraries for the game
+   */
+  async loadLibraries () {
+    if (this.gameLibraries) {
+      for (let library of this.gameLibraries) {
+        // Will check if the library needs to be imported from a cdn, or import it from the game repo directly.
+        await import(/* webpackIgnore: true */ library.cdn ? library.url : this.generateRawUrl(library.url))
+      }
+    } else {
+      console.info('No libraries to load')
+    }
+  }
+
   /**
    * Loads the game class
    */
