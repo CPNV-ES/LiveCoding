875df97479b1a85d33f970b9ed1626640e9c0db6
bastien <bastien.nicoud@gmail.com>
2019-02-15 20:04:22 +0100

    Add method to check the languages support on a game manifest

--- client/src/game/GameManifestValidator.js
+++ client/src/game/GameManifestValidator.js
@@ -1,3 +1,8 @@
+import availableLanguages from '@/processor/AvailableLanguages'
+
+/**
+ * Responsible to validates the manifest.json style
+ */
 export class GameManifestValidator {
   /**
    * Constructor
@@ -5,6 +10,7 @@ export class GameManifestValidator {
    */
   constructor (manifest) {
     this.manifest = manifest
+    this.formatChecked = false
   }
 
   /**
@@ -13,7 +19,7 @@ export class GameManifestValidator {
    * @throws {Error}
    */
   checkFormat () {
-
+    this.formatChecked = true
   }
 
   /**
@@ -22,6 +28,16 @@ export class GameManifestValidator {
    * @throws {Error}
    */
   checkLanguages () {
-
+    if (this.formatChecked) {
+      // Check if each manifest languages are included in the availableLanguages constant
+      if (Object.keys(this.manifest.interpreters).every(e => availableLanguages.includes(e))) {
+        console.log('Tous les languanges sont suportés')
+        return true
+      } else {
+        throw new Error(`Some specified languages are not suported by the platform, supported languages : ${availableLanguages.toString()}`)
+      }
+    } else {
+      throw new Error('You need to check the manifest format before checking the languages !')
+    }
   }
 }
