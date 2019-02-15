da9c9a7b9b617a2b0b7d2617607a544ad3ba1f90
bastien <bastien.nicoud@gmail.com>
2019-02-15 19:32:03 +0100

    Create new manifest validator class

---
+++ client/src/game/GameManifestValidator.js
@@ -0,0 +1,27 @@
+export class GameManifestValidator {
+  /**
+   * Constructor
+   * @param {Object} manifest The manifest parsed
+   */
+  constructor (manifest) {
+    this.manifest = manifest
+  }
+
+  /**
+   * Validates the manifest
+   * @returns {Boolean}
+   * @throws {Error}
+   */
+  checkFormat () {
+
+  }
+
+  /**
+   * Validates the manifest specified languages are compatibles
+   * @returns {Boolean}
+   * @throws {Error}
+   */
+  checkLanguages () {
+
+  }
+}
