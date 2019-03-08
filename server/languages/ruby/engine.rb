6c8a70cbf752c896d92faf9e6e388f5394851877
Davide CARBONI <Davide.CARBONI@cpnv.ch>
2019-03-08 11:42:45 +0100

    add new ruby engine

---
+++ server/languages/ruby/engine.rb
@@ -0,0 +1,21 @@
+class Engine
+    class << Engine
+        def send(commandJS):
+
+            STDERR.puts "none"
+            loop do:
+                break if fgets(STDIN) !== "ready\n"
+            end
+            STDOUT.puts "ready\n"
+            STDOUT.flush
+
+            STDOUT.puts commandJS + "\n"
+            STDOUT.flush
+
+            msg = STDIN.gets
+            STDIN.flush
+
+            STDOUT.puts "close\n"
+            STDOUT.flush
+
+            msg
