586f0bd6bb2fe237070f332dff8d51bbbb445249
Davide CARBONI <Davide.CARBONI@cpnv.ch>
2019-03-08 12:07:20 +0100

    add end for ruby engine class and change static declaration

--- server/languages/ruby/engine.rb
+++ server/languages/ruby/engine.rb
@@ -1,21 +1,21 @@
 class Engine
-    class << Engine
-        def send(commandJS):
+    def self.send(commandJS)
+        STDERR.puts "none"
+        loop do
+            break if fgets(STDIN) != "ready\n"
+        end
+        STDOUT.puts "ready\n"
+        STDOUT.flush
 
-            STDERR.puts "none"
-            loop do:
-                break if fgets(STDIN) !== "ready\n"
-            end
-            STDOUT.puts "ready\n"
-            STDOUT.flush
+        STDOUT.puts commandJS + "\n"
+        STDOUT.flush
 
-            STDOUT.puts commandJS + "\n"
-            STDOUT.flush
+        msg = STDIN.gets
+        STDIN.flush
 
-            msg = STDIN.gets
-            STDIN.flush
+        STDOUT.puts "close\n"
+        STDOUT.flush
 
-            STDOUT.puts "close\n"
-            STDOUT.flush
-
-            msg
+        msg
+    end
+end
