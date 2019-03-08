d269e5e3b571b3528cd2ec8ec28ca92363b8a48e
bastien <bastien.nicoud@gmail.com>
2019-03-08 14:33:25 +0100

    Add websocket protocol to WebSocketProcessorProxy

--- client/src/processor/ExternalWebSocketProcessorProxy.js
+++ client/src/processor/ExternalWebSocketProcessorProxy.js
@@ -31,65 +31,48 @@ export class ExternalWebSocketProcessorProxy {
       }
 
       // On connexion opening, launch protocol procedure
-      this.socket.onopen
-    })
-    // Launch the code interpretation
-    try {
-      // window.processor = new ProcessorConnexion()
-      let socket = new WebSocket(this.processorUrl)
-      socket.onopen = (oEvent) => {
-        console.log(oEvent)
+      this.socket.onopen = (oEvent) => {
         this.dispatch('console/success', 'Processor connected, sending data !')
-        // step 1 : sending language
-        socket.send(this.language)
-        socket.onmessage = (mEvent) => {
+        this.socket.send(this.language)
+        this.socket.onmessage = (mEvent) => {
           if (mEvent.data === 'OK') {
-            socket.send(this.interpreter)
-            socket.onmessage = (mEvent) => {
+            this.socket.send(this.interpreter)
+            this.socket.onmessage = (mEvent) => {
               if (mEvent.data === 'OK') {
-                socket.send(this.userScript)
-                socket.onmessage = (mEvent) => {
+                this.socket.send(this.userScript)
+                this.socket.onmessage = (mEvent) => {
                   if (mEvent.data === 'OK') {
                     this.dispatch('console/success', 'Processor successfully loaded script, launching process.')
                     this.dispatch('console/info', 'Waiting for engine interactions')
-                    socket.onmessage = (mEvent) => {
+                    this.socket.onmessage = (mEvent) => {
                       // Evaluate the command in the game context
-                      let tutu = window.game.executeGameCommand(mEvent.data)
-                      socket.send(tutu)
+                      if (mEvent.data === 'close game') {
+                        this.socket.close()
+                      } else {
+                        let tutu = window.game.executeGameCommand(mEvent.data)
+                        this.socket.send(tutu)
+                      }
                     }
                   } else {
-                    socket.close()
+                    this.socket.close()
                   }
                 }
               } else {
-                socket.close()
+                this.socket.close()
               }
             }
           } else {
-            socket.close()
+            this.socket.close()
           }
         }
       }
-      socket.onclose = (cEvent) => {
-        this.dispatch('console/info', 'Websocket connexion closed.')
-        console.log(cEvent)
-      }
-      socket.onerror = (eEvent) => {
-        this.dispatch('console/error', 'Websocket connexion error.')
-        console.log(eEvent)
-        throw new Error('Websocket connexion error !')
-      }
-    } catch (e) {
-      console.error(e)
-      this.dispatch('console/error', 'Error during running !')
-      throw new Error()
-    }
+    })
   }
 
   /**
    * Close the connexion and stop the process execution
    */
   stopExecution () {
-
+    this.socket.close()
   }
 }
